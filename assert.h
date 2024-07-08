c1b1ff3ee7a70e84f040909b12b5cf1e251ca17f
Daniel McBrearty <danielmcbrearty@gmail.com>
2024-07-08 09:07:40 +0200

    create error code in assert.h

--- assert.h
+++ assert.h
@@ -6,11 +6,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifndef SOURCE_ID
+  #error SOURCE_ID is not defined (the source file that includes this file should have been found by make_source_ids.py)
+#endif
+
+#define ERROR_CODE (( SOURCE_ID << 16 ) + __LINE__ )
+
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
 #define ASSERT(condition)  \
     if (!(condition)) { \
-        printf("ASSERT at %s:%d\n", __FILENAME__, __LINE__ ); \
+        printf("ASSERT at %s:%d - ERROR_CODE is 0x%08x\n", __FILENAME__, __LINE__, ERROR_CODE ); \
         exit(-1); \
     }
 
